53559a63ab2efa9da5ca2e9f2da2e8efa73b88cd
YazeedGhazalat <yazeedghazalat@gmail.com>
2022-09-05 12:45:46 +0300

    this is 2ed update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
         body: ListView(
           //scrollDirection: Axis.horizontal,
           children: [
-            Image(
+            Image(height: 500,
+            width: 500,
               image: AssetImage("images/1.jpg"),
             ),
             RaisedButton(
@@ -25,78 +26,56 @@ class MyApp extends StatelessWidget {
               child: Text("press me"),
               // disabledColor: Colors.amber,
               // disabledTextColor: Colors.black,
-              onPressed: () => print("im 1 but"),
+              onPressed: () => print("im Button 1"),
               color: Colors.red,
               onLongPress: () {},
             ),
             Image(
+              
               image: AssetImage("images/2.jpg"),
             ),
-            Row(children: [ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shadowColor: Colors.purpleAccent,
-                  elevation: 40,
-                  alignment: Alignment.center,
-                  
-                 // fixedSize: Size(20, 20),
-                  minimumSize: Size(1, 50),
-                  primary: Colors.blue[900],
-                  onPrimary: Colors.white),
-              onPressed: () => print("im 2 but"),
-              child: Text("press me two"),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shadowColor: Colors.purpleAccent,
-                  elevation: 40,
-                  alignment: Alignment.center,
-                  
-                 // fixedSize: Size(20, 20),
-                  minimumSize: Size(1, 50),
-                  primary: Colors.blue[900],
-                  onPrimary: Colors.white),
-              onPressed: () => print("im 2 but"),
-              child: Text("press me two"),
-            ),],),
             
-            Image(
-              image: AssetImage("images/3.jpg"),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.purpleAccent,
+                      elevation: 40,
+                      alignment: Alignment.center,
+
+                      // fixedSize: Size(20, 20),
+                      minimumSize: Size(1, 50),
+                      primary: Colors.blue[900],
+                      onPrimary: Colors.white),
+                  onPressed: () => print("im Button 2"),
+                  child: Text("press me im 2"),
+                
+              
             ),
-            Column(
-              children: [
-                Card(
-                  elevation: 25,
-                  shadowColor: Colors.blue,
-                  color: Colors.blue[100],
-                  child: Text(
-                    "horse",
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
-                Card(
-                  elevation: 25,
-                  shadowColor: Colors.amber,
-                  color: Colors.amber[100],
-                  child: Text(
-                    "snake",
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
-                Card(
-                  elevation: 25,
-                  shadowColor: Colors.red,
-                  color: Colors.red[100],
-                  child: Text(
-                    "dear",
-                    style: TextStyle(fontSize: 40),
-                  ),
-                )
-              ],
+            Image(height: 500,
+            width: 500,
+              image: AssetImage("images/3.jpg"),
+              
+            ),
+            
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.purpleAccent,
+                      elevation: 40,
+                      alignment: Alignment.center,
+
+                      // fixedSize: Size(20, 20),
+                      minimumSize: Size(1, 50),
+                      primary: Colors.blue[900],
+                      onPrimary: Colors.white),
+                  onPressed: () => print("im Button 3"),
+                  child: Text("press me im 3"),
+                
+               
+              
             ),
             Divider(
               color: Colors.amber,
               thickness: 5,
-            )
+            ),
           ],
         ),
       ),
